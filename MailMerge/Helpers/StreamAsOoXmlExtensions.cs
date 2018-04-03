55498731c90ce0917aeaff7fe7b5e14eb7f7f3b2
Chris.Carroll <chris.carroll@unforgettable.me.uk>
2018-04-03 21:31:58 +0100

    Comment on StreamAsOoXmlExtensions.AsXPathDocOfWordprocessingMainDocument()

--- MailMerge/Helpers/StreamAsOoXmlExtensions.cs
+++ MailMerge/Helpers/StreamAsOoXmlExtensions.cs
@@ -20,6 +20,21 @@ namespace MailMerge.Helpers
             var docAsXPath = new XPathDocument(source.AsWordprocessingDocument().MainDocumentPart.GetStream(FileMode.Open,fileAccess));
             return docAsXPath;
         }
+
+        /// <summary> XPath query example using LinqToXml / XElement:
+        /// <code>using (WordprocessingDocument wordDocument = WordprocessingDocument.Open(filename, true, openSettings))
+        /// {
+        ///   var xe= XElement.Load(wordDocument.MainDocumentPart.GetStream());
+        ///   xe.XPathSelectElements("//w:instrText", OoXmlNamespaces.Manager);
+        ///   ... etc ...
+        /// 
+        /// wordDocument.SaveAs(  Path.Combine(new FileInfo(filename).DirectoryName, new FileInfo(filename).Name + " Output.docx" ) );
+        /// }</code>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="isEditable"></param>
+        /// <returns></returns>
         public static XElement AsXElementOfWordprocessingMainDocument(this Stream source, bool isEditable=false)
         {
             var fileAccess =  isEditable?FileAccess.ReadWrite: FileAccess.Read;
